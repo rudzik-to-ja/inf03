2b6cf10e1636a925ccda382483d216695c2d36cf
rudzik-to-ja <145573248+rudzik-to-ja@users.noreply.github.com>
2025-05-21 09:30:42 +0200

    html inf03 2025

---
+++ INF.03-12-25.01/piekarnia.php
@@ -0,0 +1,79 @@
+<!DOCTYPE html>
+<html lang="pl">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PIEKARNIA</title>
+    <link rel="stylesheet" href="styles.css">
+    </link>
+</head>
+
+<body>
+    <div class="Nawigacja">
+        <a href="">KWERENDA1</a>
+        <a href="">KWERENDA2</a>
+        <a href="">KWERENDA3</a>
+        <a href="">KWERENDA4</a>
+    </div>
+    <header>
+        <h1>WITAMY</h1>
+        <h4>NA STRONIE PIEKARNI</h4>
+        <p>Od 31 lat oferujemy najwyższej jakości pieczywo. Naturalnie świeże, naturalnie smaczne. Pieczemy wyłącznie
+            wypieki na naturalnym zakwasie bez polepszaczy i zagęstników. Korzystamy wyłącznie z najlepszych ziaren
+            pochodzących z ekologicznych upraw położonych w rejonach zgierskim i ozorkowskim.</p>
+    </header>
+    <main>
+        <h4>Wybierz rodzaj wypieków:</h4>
+        <form action="">
+            <select name="" id="">
+                <option value=""></option>
+            </select>
+            <button>Wybierz</button>
+        </form>
+        <table>
+            <tr>
+                <td>Rodzaj</td>
+                <td>Nazwa</td>
+                <td>Gramatura</td>
+                <td>Cena</td>
+            </tr>
+            <tr>
+                <td></td>
+                <td></td> <!-- TODO: reszta wiersz wypełniona skryptem -->
+                <td></td>
+                <td></td>
+            </tr>
+            <tr>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+            </tr>
+            <tr>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+            </tr>
+            <tr>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+            </tr>
+            <tr>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+            </tr>
+        </table>
+    </main>
+    <foother>
+        <p>Autor:00000000000</p>
+        <p>Data: 21.05.2025</p>
+    </foother>
+</body>
+
+</html>
